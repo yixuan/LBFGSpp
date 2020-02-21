e14d5782f5cab3d5f2548898f759f6b06854ade3
Yixuan Qiu <yixuanq@gmail.com>
2020-02-21 12:46:04 -0500

    more functions for the BFGS matrix

--- include/LBFGS/BFGSMat.h
+++ include/LBFGS/BFGSMat.h
@@ -5,11 +5,17 @@
 #define BFGS_MAT_H
 
 #include <Eigen/Core>
+#include <Eigen/LU>
 
 
 namespace LBFGSpp {
 
 
+// An *implicit* representation of the BFGS approximation to the Hessian matrix B
+// B = theta * I - W * M * W'
+// H = inv(B)
+// See [1] D. C. Liu and J. Nocedal (1989). On the limited memory BFGS method for large scale optimization.
+//     [2] R. H. Byrd, P. Lu, and J. Nocedal (1995). A limited memory algorithm for bound constrained optimization.
 template <typename Scalar>
 class BFGSMat
 {
@@ -32,6 +38,8 @@ private:
                      //          then in the next iteration m_s[, 0] will be overwritten.
                      //          m_s[, m_ptr-1] points to the most recent history,
                      //          and m_s[, m_ptr % m] points to the most distant one.
+
+    Eigen::PartialPivLU<Matrix> m_Msolver;
 
 public:
     // Constructor
@@ -72,11 +80,13 @@ public:
         m_ptr = loc + 1;
     }
 
-    // Recursive formula to compute a * H * v, where a is a scalar
+    // Recursive formula to compute a * H * v, where a is a scalar, and v is [n x 1]
     // H0 = (1/theta) * I is the initial approximation to H
     // Algorithm 7.4 of Nocedal, J., & Wright, S. (2006). Numerical optimization.
     inline void apply_Hv(const Vector& v, const Scalar& a, Vector& res)
     {
+        res.resize(v.size());
+
         // L-BFGS two-loop recursion
 
         // Loop 1
@@ -99,6 +109,108 @@ public:
             res.noalias() += (m_alpha[j] - beta) * m_s.col(j);
             j = (j + 1) % m_m;
         }
+    }
+
+    // W = [Y, theta * S]
+    // W is [n x (2 * ncorr)], v is [n x 1]
+    inline void apply_Wtv(const Vector& v, Vector& res)
+    {
+        res.resize(2 * m_ncorr);
+
+        // To iterate from the most recent history to most distant,
+        // j = ptr - 1
+        // for i = 0, ..., ncorr
+        //     j points to the column of y or s that is i away from the most recent one
+        //     j = (j + m - 1) % m
+        // end for
+
+        // Pointer to most recent history
+        int j = m_ptr - 1;
+        for(int i = 0; i < m_ncorr; i++)
+        {
+            // res[0] corresponds to the oldest y
+            // res[ncorr-1]   => newest y
+            // res[ncorr]     => oldest s
+            // res[2*ncorr-1] => newest s
+            res[m_ncorr - 1 - i] = m_y.col(j).dot(v);
+            res[2 * m_ncorr - 1 - i] = m_theta * m_s.col(j).dot(v);
+            j = (j + m_m - 1) % m_m;
+        }
+    }
+
+    inline void form_M()
+    {
+        if(m_ncorr < 1)
+            return;
+
+        // Minv = [-D         L']
+        //        [ L  theta*S'S]
+        Matrix Minv = Matrix::Zero(2 * m_ncorr, 2 * m_ncorr);
+        // Pointer to most recent history
+        const int loc = m_ptr - 1;
+        // Segment 1: 0, 1, ..., loc
+        // Copy to ncorr-len1, ..., ncorr-1
+        const int len1 = loc + 1;
+        // Segment 2: loc+1, ..., m-1, only when ncorr == m
+        // Copy to 0, 1, ..., len2-1
+        const int len2 = m_m - loc - 1;
+
+        // Copy -D
+        // To iterate from the most recent history to most distant,
+        // j = ptr - 1
+        // for i = 0, ..., ncorr
+        //     j points to the column of y or s that is i away from the most recent one
+        //     j = (j + m - 1) % m
+        // end for
+        int i = 0, j = m_ptr - 1;
+        for(i = 0; i < m_ncorr; i++)
+        {
+            Minv(m_ncorr - i - 1, m_ncorr - i - 1) = m_ys[j];
+            j = (j + m_m - 1) % m_m;
+        }
+        // Another implementation
+        // Segment 1
+        // Minv.diagonal().segment(m_ncorr - len1, len1).noalias() = -m_ys.head(len1);
+        // Segment 2
+        // if(m_ncorr == m_m && len2 > 0)
+        // {
+        //     Minv.diagonal().head(len2).noalias() = -m_ys.tail(len2);
+        // }
+
+        // Compute L
+        // Let S=[s[0], ..., s[m-1]], Y=[y[0], ..., y[m-1]]
+        // L = [          0                                     ]
+        //     [  s[1]'y[0]             0                       ]
+        //     [  s[2]'y[0]     s[2]'y[1]                       ]
+        //     ...
+        //     [s[m-1]'y[0] ... ... ... ... ... s[m-1]'y[m-2]  0]
+        //
+        // We only use y up to y[m-2]
+        int jloc = m_ptr - 1;
+        jloc = (jloc + m_m - 1) % m_m;
+        for(j = m_ncorr - 2; j >= 0; j--)
+        {
+            int iloc = m_ptr - 1;
+            for(i = m_ncorr - 1; i > j; i--)
+            {
+                Minv(m_ncorr + i, j) = m_y.col(jloc).dot(m_s.col(iloc));
+                iloc = (iloc + m_m - 1) % m_m;
+            }
+            jloc = (jloc + m_m - 1) % m_m;
+        }
+        // Copy to the top right corner
+        Minv.topRightCorner(m_ncorr, m_ncorr).noalias() = Minv.bottomLeftCorner(m_ncorr, m_ncorr).transpose();
+
+        // Compute theta*S'S
+        Minv.bottomRightCorner(len1, len1).noalias() = m_theta * m_s.leftCols(len1).transpose() * m_s.leftCols(len1);
+        if(m_ncorr == m_m && len2 > 0)
+        {
+            Minv.block(m_ncorr, m_ncorr, len2, len2).noalias() = m_theta * m_s.rightCols(len2).transpose() * m_s.rightCols(len2);
+            Minv.block(m_ncorr + len2, m_ncorr, len1, len2).noalias() = m_theta * m_s.leftCols(len1).transpose() * m_s.rightCols(len2);
+            Minv.block(m_ncorr, m_ncorr + len2, len2, len1).noalias() = Minv.block(m_ncorr + len2, m_ncorr, len1, len2).transpose();
+        }
+
+        m_Msolver.compute(Minv);
     }
 };
 
