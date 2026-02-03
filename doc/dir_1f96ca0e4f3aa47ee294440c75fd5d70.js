dd11e0315ebcb2b5abc5d78c76431e1c7db90f58
Yixuan Qiu <yixuanq@gmail.com>
2026-02-03 16:49:01 +0800

    remove debugging files

--- doc/dir_1f96ca0e4f3aa47ee294440c75fd5d70.js
+++ doc/dir_1f96ca0e4f3aa47ee294440c75fd5d70.js
@@ -6,8 +6,6 @@ var dir_1f96ca0e4f3aa47ee294440c75fd5d70 =
     [ "LineSearchBacktracking.h", "LineSearchBacktracking_8h_source.html", null ],
     [ "LineSearchBracketing.h", "LineSearchBracketing_8h_source.html", null ],
     [ "LineSearchMoreThuente.h", "LineSearchMoreThuente_8h_source.html", null ],
-    [ "LineSearchMoreThuente_1.h", "LineSearchMoreThuente__1_8h_source.html", null ],
-    [ "LineSearchMoreThuente_2.h", "LineSearchMoreThuente__2_8h_source.html", null ],
     [ "LineSearchNocedalWright.h", "LineSearchNocedalWright_8h_source.html", null ],
     [ "Param.h", "Param_8h_source.html", null ],
     [ "SubspaceMin.h", "SubspaceMin_8h_source.html", null ]
